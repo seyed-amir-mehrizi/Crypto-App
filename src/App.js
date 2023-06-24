5341a2b48b99b41c6c9f96e4e05e1c12d912d2a7
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2023-06-24 22:54:38 +0430

    add navbar tp the app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
+import { Layout, Space, Typography } from 'antd';
+import { Navbar } from './components'
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+
+      </div>
+      <div className='footer'>
+
+      </div>
     </div>
   );
 }
