8e7a9711fab578c795358793c05e8c12440e7302
Amir Mehrizi <a.mehrizi@PIKAD.COM>
2023-07-09 21:16:44 +0330

    add components and footer to the app.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
 import { Layout, Space, Typography } from 'antd';
-import { Navbar } from './components'
+import { Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
+
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+
 function App() {
   return (
     <div className="app">
@@ -9,10 +12,44 @@ function App() {
         <Navbar />
       </div>
       <div className='main'>
-
-      </div>
-      <div className='footer'>
-
+        <Layout>
+          <div className='routes'>
+            <Switch>
+              <Route exact path='/'>
+                <HomePage />
+              </Route>
+              <Route path='/exchanges'>
+                <Exchanges />
+              </Route>
+              <Route path='/cryptocurrencies'>
+                <Cryptocurrencies />
+              </Route>
+              <Route path='/crypto/:coinId'>
+                <CryptoDetails />
+              </Route>
+              <Route path='/news'>
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+        <div className='footer' >
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>
+              Homeّ
+            </Link>
+            <Link to='/exchanges'>
+              Exchanges
+            </Link>
+            <Link to='/news'>
+              News
+            </Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
